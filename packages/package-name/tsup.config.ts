e8c0253ae403e5624bed272750be5670c43d5fac
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-02-17 15:57:51 +0800

    refactor: monorepo

--- packages/package-name/tsup.config.ts
+++ packages/package-name/tsup.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'tsup'
 
 export const tsup = defineConfig((option) => ({
   entry: ['src/index.ts'],
-  dts: !option.watch,
+  dts: true,
   clean: true,
   format: ['cjs', 'esm'],
+  platform: 'node',
+  splitting: false,
+  treeshake: true,
   minify: false,
   sourcemap: !!option.watch,
-  tsconfig: option.watch ? './tsconfig.dev.json' : './tsconfig.json',
+  tsconfig: option.watch ? '../../tsconfig.dev.json' : '../../tsconfig.json',
 }))
