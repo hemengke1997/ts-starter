54a4105bd621c63643696bd3c68f8765844f8d16
hemengke1997 <23536175@qq.com>
2023-10-24 16:18:45 +0800

    perf: improve infrastructure

--- playground/spa/src/App.tsx
+++ playground/spa/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
