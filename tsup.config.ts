d5d0fbbc347d9b6378ad2564ac3d4965650bed5d
hemengke <23536175@qq.com>
2024-02-06 15:37:27 +0800

    feat: Integrated changesets, turbo

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'tsup'
 export const tsup = defineConfig((option) => ({
   entry: ['src/index.ts'],
   dts: true,
-  clean: true,
   format: ['cjs', 'esm'],
   platform: 'node',
   splitting: false,
   treeshake: true,
-  minify: false,
+  clean: !option.watch,
+  minify: !option.watch,
   sourcemap: !!option.watch,
   tsconfig: option.watch ? 'tsconfig.dev.json' : 'tsconfig.json',
 }))
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    onConsoleLog(log) {
-      if (log.includes('Generated an empty chunk')) {
-        return false
-      }
-      return undefined
-    },
-  },
+  test: {},
 })
